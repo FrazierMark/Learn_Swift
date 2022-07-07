54f17f598b566a5ff034f21fbafd8c25e7c8d289
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-07-07 08:34:34 -0700

    Initial commit

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Mark Frazier on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
