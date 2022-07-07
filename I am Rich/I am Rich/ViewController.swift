620cfa543c27764d5a2a1dbd33b97d5817bb9520
FrazierMark <m.mark.frazier@gmail.com>
2022-07-07 13:52:19 -0700

    Starting Udemy iOS course

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mark Frazier on 7/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
