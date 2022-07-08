3ae2a29c58898cc18a6bf8dfe8eb1ddec0232bdd
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-07-08 13:43:53 -0700

    Add randomization when roll button pressed

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -7,21 +7,33 @@ class ViewController: UIViewController {
     // Interface builder outlet (a way to access/reference this particular UI element)
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
+   
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // Block of code runs when app is loaded
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    // Interface Builder Action/Function
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ][Int.random(in: 0...5)]
         
-        // Change diceImg1 to diceImg6
-        diceImageView1.image = UIImage(named: "DiceSix")
-        // diceImageView1.alpha = 0.5
-        
-        // Who        //What        Value
-        diceImageView2.image = UIImage(named: "DiceTwo")
-        
+        diceImageView2.image = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ][Int.random(in: 0...5)]
         
     }
-
-
 }
 
