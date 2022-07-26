dc6598e9d8e06d245f61f655e68027ccd8767c20
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-07-26 09:06:46 -0700

    Started 2 tutorials and they were not good, continuing with udemy course

---
+++ H4X0R News/H4X0R News/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Mark Frazier on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+        List(posts) { post in
+            Text(post.title).padding()
+        }
+        .navigationBarTitle("H4X0R NEWS")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+// Post conforms to the Identifiable protocol\
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+
+let posts = [
+    Post(id: "1", title: "hello"),
+    Post(id: "2", title: "bonjour"),
+    Post(id: "3", title: "hola")
+]
